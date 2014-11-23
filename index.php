fb0fbd53ba39e2b1bc5b33f116e6ea3f0a62060b
Nathan Moeller <ndmoeller@wisc.edu>
2014-11-23 11:01:23 -0600

    added database file, index page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Soccer Application</title>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+</head>
+<body>
+<div class="container">
+	<h1>Soccer Application</h1>
+	<div class="row">
+		below is how we do a table
+	</div>
+	<div class="row">
+		<table class="table table-striped">
+			<thead>
+				<th>col1</th>
+				<th>col2</th>
+				<th>col3</th>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>5</td>
+					<td>6</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+</body>
+</html>
