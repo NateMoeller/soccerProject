ecabb74492ecf728eee1242e68db05686f9ee8d2
Nathan Moeller <ndmoeller@wisc.edu>
2014-11-25 22:21:39 -0600

    select league, option, and sort works

--- index.php
+++ index.php
@@ -3,8 +3,41 @@ $leagues = ['Premier League', 'Bundesliga 1', 'La Liga', 'Serie A', 'Le Champion
 $options = ['Wins', 'Loses', 'Draws', 'Goals', 'Shots', 'Shots on Target', 'Yellow cards', 'Red Cards', 'Fouls'];
 
 $selLeague = isset($_POST['league']) ? $_POST['league'] : '';
-$selOption = isset($_POST['option']) ? $_POST['option'] : '';
+$selOption = isset($_POST['option']) ? strtolower($_POST['option']) : '';
 $selSort = isset($_POST['sort']) ? $_POST['sort'] : 'most';
+
+//process option
+if($selOption == "goals"){
+	$proOption = "goals";
+}
+else if($selOption == "shots"){
+	$proOption = "shots";
+}
+else if($selOption == "shots on target"){
+	$proOption = "shots on target";
+}
+else if($selOption == "yellow cards"){
+	$proOption = "yellows";
+}
+else if($selOption == "red cards"){
+	$proOption = "reds";
+}
+else if($selOption == "fouls"){
+	$proOption = "fouls";
+}
+
+//process sort
+if($selSort == 'most'){
+	$sort = "DESC";
+}
+else{
+	$sort = "ASC";
+}
+
+//right now only season 2014/15 works
+$season2014_15 = isset($_POST['2014/15']) ? $_POST['2014/15'] : '';
+
+$con = mysqli_connect("localhost","root","", "soccer");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,23 +51,26 @@ $selSort = isset($_POST['sort']) ? $_POST['sort'] : 'most';
 <div class="container">
 	<h1>Soccer Application</h1>
 	<div class="row">
+		<div class="col-md-6" style="float:left;">
 		<?php
 		//echo out our form
 		echo "<form action=\"index.php\" method=\"POST\">";
 		echo "Select a league: ";
 		echo "<select id=\"league\" name=\"league\" class=\"form-control\" style=\"width: 500px;\">";
+		$id = 1;
 		foreach($leagues as $league){
-			echo "<option value='$league'>$league</option>";
+			echo "<option value=$id>$league</option>";
+			$id++;
 		}
 		echo "</select>";
 		
 				
 		echo "<br/>Season:<br/>";
-		echo "<label><input type=\"checkbox\" value=\"\"> 2014/2015 </label><br/>";
-		echo "<label><input type=\"checkbox\" value=\"\"> 2013/2014 </label><br/>";
-		echo "<label><input type=\"checkbox\" value=\"\"> 2012/2013 </label><br/>";
-		echo "<label><input type=\"checkbox\" value=\"\"> 2011/2012 </label><br/>";
-		echo "<label><input type=\"checkbox\" value=\"\"> 2010/2011 </label><br/>";
+		echo "<label class=\"checkbox-inline\"><input name=\"2014/15\" type=\"checkbox\" value=\"2014/15\">2014/2015</label>";
+		echo "<label class=\"checkbox-inline\"><input type=\"checkbox\" value=\"\">2013/2014</label>";
+		echo "<label class=\"checkbox-inline\"><input type=\"checkbox\" value=\"\">2012/2013</label>";
+		echo "<label class=\"checkbox-inline\"><input type=\"checkbox\" value=\"\">2011/2012</label>";
+		echo "<label class=\"checkbox-inline\"><input type=\"checkbox\" value=\"\">2010/2011</label>";
 		
 		echo "<br/>Select:"; 
 		echo "<select id=\"option\" name=\"option\" class=\"form-control\" style=\"width: 500px;\">";
@@ -54,15 +90,41 @@ $selSort = isset($_POST['sort']) ? $_POST['sort'] : 'most';
 		echo "<button type=\"submit\" class=\"btn btn-default\">Submit</button>";
 		echo "</form>";
 		?>
-	</div>
-	<div class="row">
-		<?php
+		</div>
+		<div class="col-md-6" style="float:left;">
+			<?php
 			echo "League: " . $selLeague . "<br/>";
 			echo "Option: " . $selOption . "<br/>";
-			echo "Sort by: " . $selSort . "<br/>";
-		?>
+			echo "Sort by: " . $selSort . "<br/><br/>";
+			
+			
+			if(isset($_POST['league'])){
+				//execute the query
+	
+				//count query
+				$query = "SELECT `T_name`, SUM(`" . $proOption . "`) AS Goals FROM  `teamgame` NATURAL JOIN  `team` WHERE team_id = T_id AND L_id = $selLeague AND 
+				season_id=5 GROUP BY `T_name` ORDER BY Goals " . $sort . ";";
+				$result = mysqli_query($con, $query);
+				if(!$result){
+					echo "query did not work";
+				}
+				echo "<table class=\"table table-striped\">";
+				echo "<thead><th>Team Name</th><th>$selOption</th></thead>"; //TODO: the goals will change
+				echo "<tbody>";
+				while($row = mysqli_fetch_assoc($result)){
+					echo "<tr>";
+					echo "<td>" . $row['T_name'] . "</td><td>" . $row['Goals'] . "</td>";
+					echo "</tr>";
+				}
+				echo "</tbody>";
+				echo "</table>";
+			}
+			?>
+		</div>
+	</div>
+	<div class="row">
 		<!--
-		<table class="table table-striped">
+		
 			<thead>
 				<th>col1</th>
 				<th>col2</th>
