b103d6ed62070e3392d68086ff14ab0a4efdf49d
Nathan Moeller <ndmoeller@wisc.edu>
2014-11-26 12:54:49 -0600

    added average Queries

--- avgQueries.php
+++ avgQueries.php
@@ -1,31 +1,32 @@
 <?php
 $leagues = ['Premier League', 'Bundesliga 1', 'La Liga', 'Serie A', 'Le Championnat'];
-$options = ['Wins', 'Loses', 'Draws', 'Goals', 'Shots', 'Shots on Target', '% shots on target', 'Yellow cards', 'Red Cards', 'Fouls', 'Corners'];
+$options = ['Average Goals', 'Average Shots', 'Average Shots on Target', 'Average Yellow cards', 'Average Red Cards', 'Average Fouls', 'Average Corners'];
 
 $selLeague = isset($_POST['league']) ? $_POST['league'] : '';
 $selOption = isset($_POST['option']) ? strtolower($_POST['option']) : '';
 $selSort = isset($_POST['sort']) ? $_POST['sort'] : 'most';
 
 //process option
-if($selOption == "goals"){
+//process option
+if($selOption == "average goals"){
 	$proOption = "goals";
 }
-else if($selOption == "shots"){
+else if($selOption == "average shots"){
 	$proOption = "shots";
 }
-else if($selOption == "shots on target"){
+else if($selOption == "average shots on target"){
 	$proOption = "shots on target";
 }
-else if($selOption == "yellow cards"){
+else if($selOption == "average yellow cards"){
 	$proOption = "yellows";
 }
-else if($selOption == "red cards"){
+else if($selOption == "average red cards"){
 	$proOption = "reds";
 }
-else if($selOption == "fouls"){
+else if($selOption == "average fouls"){
 	$proOption = "fouls";
 }
-else if($selOption == "corners"){
+else if($selOption == "average corners"){
 	$proOption = "corners";
 }
 //process sort
@@ -70,7 +71,7 @@ $con = mysqli_connect("localhost","root","", "soccer");
 		<?php
 		echo "<br/><br/>";
 		//echo out our form
-		echo "<form action=\"index.php\" method=\"POST\">";
+		echo "<form action=\"avgQueries.php\" method=\"POST\">";
 		echo "Select a league: ";
 		echo "<select id=\"league\" name=\"league\" class=\"form-control\" style=\"width: 500px;\">";
 		$id = 1;
@@ -129,84 +130,19 @@ $con = mysqli_connect("localhost","root","", "soccer");
 				echo $title;
 			}
 			
-			//echo "League: " . $selLeague . "<br/>";
-			//echo "Option: " . $selOption . "<br/>";
-			//echo "Sort by: " . $selSort . "<br/><br/>";
-			
 			
 			if(!isset($season5) && !isset($season4) && !isset($season3) && !isset($season2) && !isset($season1)){
 				echo "Select a Season";
 			}			
-			else if ($selOption == "wins" || $selOption == "loses" || $selOption == "draws"){
-				if($selOption == "wins"){
-					$gameResult = 1;
-				}
-				else if ($selOption == "loses"){
-					$gameResult = -1;
-				}
-				else{
-					$gameResult = 0;
-				}
-				
-				//SUM query
-				$query = "SELECT `T_name`, COUNT(result) AS Result FROM `teamgame` NATURAL JOIN `team` WHERE team_id = T_id AND L_id = $selLeague AND ("; 
-				$query .= (isset($season5)) ? "season_id = $season5 OR ": '';
-				$query .= (isset($season4)) ? "season_id = $season4 OR ": '';
-				$query .= (isset($season3)) ? "season_id = $season3 OR ": '';
-				$query .= (isset($season2)) ? "season_id = $season2 OR ": '';
-				$query .= (isset($season1)) ? "season_id = $season1 OR ": '';
-				$query = substr($query, 0, strlen($query) - 3);
-				$query .= ") AND result = $gameResult GROUP BY team_id ORDER BY Result " . $sort . ";";
-				$result = mysqli_query($con, $query);
-				if(!$result){
-					echo "query did not work";
-				}
-				echo "<table class=\"table table-striped\">";
-				echo "<thead><th>Team Name</th><th>$selOption</th></thead>";
-				echo "<tbody>";
-				while($row = mysqli_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td>" . $row['T_name'] . "</td><td>" . $row['Result'] . "</td>";
-					echo "</tr>";
-				}
-				echo "</tbody>";
-				echo "</table>";
-			}
-			else if($selOption == "% shots on target"){
-				$query = "SELECT `T_name`, SUM(`shots on target`) / SUM(`shots`) AS ShotsPercent FROM `teamgame` NATURAL JOIN `team` WHERE team_id = T_id
-				AND L_id = $selLeague AND (";
-				$query .= (isset($season5)) ? "season_id = $season5 OR ": '';
-				$query .= (isset($season4)) ? "season_id = $season4 OR ": '';
-				$query .= (isset($season3)) ? "season_id = $season3 OR ": '';
-				$query .= (isset($season2)) ? "season_id = $season2 OR ": '';
-				$query .= (isset($season1)) ? "season_id = $season1 OR ": '';
-				$query = substr($query, 0, strlen($query) - 3);
-				$query .= ") GROUP BY `T_name` ORDER BY ShotsPercent " . $sort . ";";
-				$result = mysqli_query($con, $query);
-				if(!$result){
-					echo "query did not work";
-				}
-				echo "<table class=\"table table-striped\">";
-				echo "<thead><th>Team Name</th><th>$selOption</th></thead>";
-				echo "<tbody>";
-				while($row = mysqli_fetch_assoc($result)){
-					echo "<tr>";
-					echo "<td>" . $row['T_name'] . "</td><td>" . $row['ShotsPercent'] . "</td>";
-					echo "</tr>";
-				}
-				echo "</tbody>";
-				echo "</table>";
-			}
 			else{
-				//SUM query
-				$query = "SELECT `T_name`, SUM(`" . $proOption . "`) AS Goals FROM  `teamgame` NATURAL JOIN `team` WHERE team_id = T_id AND L_id = $selLeague AND (";
+				$query = "SELECT `T_name`, SUM(`" . $proOption . "`) / COUNT(`game_id`) AS Avg FROM  `teamgame` NATURAL JOIN `team` WHERE team_id = T_id AND L_id = $selLeague AND (";
 				$query .= (isset($season5)) ? "season_id = $season5 OR ": '';
 				$query .= (isset($season4)) ? "season_id = $season4 OR ": '';
 				$query .= (isset($season3)) ? "season_id = $season3 OR ": '';
 				$query .= (isset($season2)) ? "season_id = $season2 OR ": '';
 				$query .= (isset($season1)) ? "season_id = $season1 OR ": '';
 				$query = substr($query, 0, strlen($query) - 3);
-				$query .= ") GROUP BY `T_name` ORDER BY Goals " . $sort . ";";
+				$query .= ") GROUP BY `T_name` ORDER BY Avg " . $sort . ";";
 				$result = mysqli_query($con, $query);
 				if(!$result){
 					echo "query did not work";
@@ -216,7 +152,7 @@ $con = mysqli_connect("localhost","root","", "soccer");
 				echo "<tbody>";
 				while($row = mysqli_fetch_assoc($result)){
 					echo "<tr>";
-					echo "<td>" . $row['T_name'] . "</td><td>" . $row['Goals'] . "</td>";
+					echo "<td>" . $row['T_name'] . "</td><td>" . $row['Avg'] . "</td>";
 					echo "</tr>";
 				}
 				echo "</tbody>";
